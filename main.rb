9f2ef8265703fa10491a8c879f14cd305825edb7
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-03-29 21:17:05 +0200

    Render the cheapest products

--- main.rb
+++ main.rb
@@ -6,3 +6,9 @@ clients = Client.all
 clients.each do |client|
     puts client.lastname + " : " + client.firstname
 end
+
+puts "--- Cheapest products ---"
+products = Product.cheap
+products.each do |product|
+    puts product.name + " : " + product.price.to_s
+end
--- models/product.rb
+++ models/product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
     belongs_to :category
     has_many :orders, through: :order_items
+
+    scope :cheap, -> { where("products.price <= ?", 20) }
+
+
 end
   
