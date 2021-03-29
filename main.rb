a6ee91fc30fb2bedc93eba54bf4624368a9e6f97
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-03-29 21:09:03 +0200

    Render clients

--- main.rb
+++ main.rb
@@ -1,3 +1,8 @@
 require_relative 'connection'
 require_relative 'models'
 
+puts "--- Render Clients ---"
+clients = Client.all
+clients.each do |client|
+    puts client.lastname + " : " + client.firstname
+end
