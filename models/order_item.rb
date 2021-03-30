4875b1e9c6cb29a8b5d27c3a5ad3b3c603a4cb86
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-03-30 21:08:27 +0200

    Set the price before_save

--- models/order_item.rb
+++ models/order_item.rb
@@ -8,9 +8,22 @@ class OrderItem < ActiveRecord::Base
     # Set a default value : In this case it'll be 100
     scope :bulk, ->(size = 100) { where('quantity >= ?', size) }
 
+    before_save :set_price
+
     # Define the price of the order_item
     def price
         quantity * item_price
+    end
+
+    protected def set_price
+        # Set the price the default value if no other values are set
+        # This option could be interesting if a price's discout exists
+        self.price ||= product.price
+        
+        # Method "unfolded"
+        # self.item_price = self.item_price || product.price
+    
+        #Note : The "self." key-word is set to use the actual variable whitout needs to create a new one
     end
 end
   
