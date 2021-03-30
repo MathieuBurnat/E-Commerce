15b66568f01ae854eec7d888a9671914744dfd24
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-03-30 18:28:49 +0200

    Add the bulk

--- main.rb
+++ main.rb
@@ -12,3 +12,10 @@ products = Product.cheap
 products.each do |product|
     puts product.name + " : " + product.price.to_s
 end
+
+quantity = 20
+puts "--- Ordered items with an amount of " + quantity.to_s + " at less ---"
+orderItems = OrderItem.bulk(quantity)
+orderItems.each do |orderItem|
+    puts orderItem.product.name + " : " + orderItem.quantity.to_s + " (Ordered by " + orderItem.order.client.firstname + " " + orderItem.order.client.lastname + ")"
+end 
