2b548691ca88d6f813dc67644edd758e6643ebc8
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-04-12 16:48:00 +0200

    Create more supplier's datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,12 +19,14 @@ Category.new(name: "Sport", description: "To all berserkers in the soul...").sav
 # Stock catageories in variable to associate them later
 it = Category.create(name: "IT", description: "Techno-Shop")
 desktop = Category.create(name: "Desktop", description: "Funny goodies :D")
+deco = Category.create(name: "Decoration", description: "Upgrade your home with beautiful decoration")
 
 # Create products (with object's associations)
 Product.create(name: "RedKeyboard", price: 98.20, description: "Look at these beautiful keys", category: it, supplier: caoutchouka)
+Product.create(name: "Start", price: 25.90, description: "Look at it it sparkles !", category: deco, supplier: nastlet)
+Product.create(name: "Josef", price: 5, description: "The Josef's poster. A2", category: deco, supplier: caoutchouka)
 
 pencil = Product.create(name: "Pencil", price: 5.10, description: "We love it.", category: desktop, supplier: nastlet)
-
 eraser = Product.create(name: "Eraser", price: 7.20, description: "THE Eraser.", category: desktop, supplier: nastlet)
 
 # Create the object with .new to update it dynamically and save it at the end
