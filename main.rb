cba0f157721dd2a6e645d20434af00c567714a0c
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-04-15 21:36:09 +0200

    Update dirties

--- main.rb
+++ main.rb
@@ -1,26 +1,26 @@
 require_relative 'connection'
 require_relative 'models'
 
-puts "---  [#{Individual.amount.to_s}] Render Individuals ---"
+puts "A ---  [#{Individual.amount.to_s}] Render Individuals ---"
 clients = Individual.all
 clients.each do |client|
     puts client.lastname + " : " + client.firstname
 end
 
-puts "---  [#{Entreprise.amount.to_s}] Render Entreprises ---"
+puts "B ---  [#{Entreprise.amount.to_s}] Render Entreprises ---"
 entreprises = Entreprise.all
 entreprises.each do |entreprise|
     puts entreprise.company_name
 end
 
-puts "--- Cheapest products ---"
+puts "C --- Cheapest products ---"
 products = Product.cheap
 products.each do |product|
     puts product.name + " : " + product.price.to_s
 end
 
 quantity = 10
-puts "--- Ordered items with an amount of " + quantity.to_s + " or more ---"
+puts "D --- Ordered items with an amount of " + quantity.to_s + " or more ---"
 orderItems = OrderItem.bulk(quantity)
 orderItems.each do |orderItem|
     puts orderItem.product.name + " : " + orderItem.quantity.to_s + " (Ordered by " + orderItem.order.client.firstname + " " + orderItem.order.client.lastname + ")"
@@ -29,7 +29,7 @@ end
 
 # Version avec détails (Exigence 3 -> last line)
 client = Client.find(1)
-puts "Cheapest products commanded by  #{client.firstname} #{client.lastname}"
+puts "E --- Cheapest products commanded by  #{client.firstname} #{client.lastname} ---"
 products = client.cheapest_ordered_items
 
 products.each do |product|
@@ -38,11 +38,11 @@ end
 
 # client = Client.where('id = ?', 1).first
 client = Client.find(1)
-puts "--- Total of the last order of the client " + client.firstname + " " + client.lastname + " ---"
+puts "F --- Total of the last client's order " + client.firstname + " " + client.lastname + " ---"
 puts client.orders.last.total_price.to_s
 
 suppliers = Supplier.all()
-puts "---  [#{Supplier.amount.to_s}] Suppliers ---"
+puts "G ---  [#{Supplier.amount.to_s}] Suppliers ---"
 suppliers.each do |supplier|
     puts "#{supplier.name} has : " 
     supplier.categories.each do |cat|
